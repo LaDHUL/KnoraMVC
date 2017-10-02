2caad555c06dd35c3736399908943ca941121430
Loïc Jaouen <loic.jaouen@unil.ch>
2017-10-02 15:10:29 +0200

    adding resource creation adding properties to existing model

--- kapi/knora.js
+++ kapi/knora.js
@@ -20,6 +20,7 @@ function Knora() {
 		this.configKnora.server.protocol + '://' +
 		this.configKnora.server.host + "/" +
 		this.configKnora.server.version + "/";
+	this.resource_types = {};
 }
 
 /**
@@ -37,7 +38,7 @@ Knora.prototype.getUrlIri = function (command, iri) {
 };
 
 Knora.prototype.getUrl = function (command, project, iri) {
-	return this.baseUrl + command + "/" + qs.escape( "http://rdfh.ch/"+ project +"/" + iri);
+	return this.baseUrl + command + "/" + qs.escape("http://rdfh.ch/" + project + "/" + iri);
 };
 
 /**
@@ -49,7 +50,7 @@ Knora.prototype.getUrl = function (command, project, iri) {
 Knora.prototype.fixCookies = function (options, req) {
 	// if auth is requested fill it in the options
 	// read `express` request header 'Authorization'
-	logdebug("req: %o", req);
+	logdebugReq("req: %o", req);
 	let auth = req.get('Authorization');
 	if (auth) {
 		_.set(options, 'headers.authorization', auth);
@@ -68,6 +69,106 @@ Knora.prototype.fixCookies = function (options, req) {
 };
 
 /**
+ * get the resource informations
+ *
+ * input: model
+ * @param model
+ */
+Knora.prototype.knora_restypes = function (model) {
+	let options = {method: 'GET'};
+	let knora = this;
+
+	return new Promise(function (fullfill, reject) {
+		// check the cache : if we know the resource, return
+		if (knora.resource_types[model.id]) {
+			fullfill(knora.resource_types[model.id]);
+			return;
+		}
+
+		// else request the information
+		options.url = knora.baseUrl + 'resourcetypes/' + qs.escape(model.id);
+		// get the resource type
+		logdebug('restype: %o', options);
+		request(options, function (error, response, body) {
+			if (error) {
+				// what kind of error is that?
+				logdebug('error: %o', error);
+				// TODO: rework the error description
+				reject(error);
+				return;
+			}
+
+			logdebug('error: %o', response.statusCode);
+			if (response.statusCode !== HttpStatus.OK) {
+				reject(Error(body));
+				return;
+			}
+
+			// map
+			let parsedBody = JSON.parse(body);
+			if (!parsedBody.restype_info || !parsedBody.restype_info.properties) {
+				// we did not find what we are looking for
+				reject(new Error("missing resource type info"));
+				return;
+			}
+
+			// walk through the returned values to store the properties' types
+			/* {
+				 "http://www.knora.org/ontology/0108#hasFamilyName" : {
+					"name": "http://www.knora.org/ontology/0108#hasFamilyName",
+					"guiorder": 1,
+					"description": "Nom.",
+					"valuetype_id": "http://www.knora.org/ontology/knora-base#TextValue",
+					"label": "Nom",
+					"vocabulary": "http://www.knora.org/ontology/0108",
+					"attributes": "maxlength=255;size=80",
+					"occurrence": "1",
+					"id": "http://www.knora.org/ontology/0108#hasFamilyName",
+					"gui_name": "text"
+				 },
+				 "http://www.knora.org/ontology/0108#hasGivenName" : { ... },
+				 ...
+			  }
+			*/
+			let restype = {};
+			logdebug('restype properties: %o', parsedBody.restype_info.properties);
+			_.forEach(parsedBody.restype_info.properties, function (element) {
+				logdebug('restype mapping: %o, %o', element.id, element);
+				restype[element.id] = element;
+			});
+
+			// fill in the top type
+			knora.resource_types[model.id] = restype;
+
+			// walk through the model to check what property is a link
+			// that should be unfolded
+			let requests = [];
+			_.forEach(model.properties, function (value, key) {
+				if (_.isArray(value)) {
+					let subrequest = knora.knora_restypes(value[1]);
+					requests.push(subrequest);
+				}
+			});
+
+			if (requests.length === 0) {
+				fullfill(restype);
+				return;
+			}
+
+			// else: requests is not empty
+			Promise.all(requests)
+				.then(function (results) {
+					_.forEach(results, function (result) {
+						logdebug("request for: ", result);
+					});
+					fullfill(knora.resource_types);
+				})
+				.catch(reject);
+		});
+	});
+};
+
+/**
  * send a request to Knora
  *
  * send should have at least:
@@ -82,7 +183,7 @@ Knora.prototype.fixCookies = function (options, req) {
  * options : options to pass over to request
  * model : model description of the object in output
  */
-Knora.prototype.knora_request = function (options, model) {
+Knora.prototype.knora_request = function (options, model, data) {
 	let knora = this;
 
 	// make this a promise to be able to chain them
@@ -90,6 +191,73 @@ Knora.prototype.knora_request = function (options, model) {
 		// for synchronizing the subrequests
 		let subrequests = [];
 		let anchors = [];
+
+		// for POST, populate data
+		if (options.method === 'POST') {
+			options.body = {};
+			options.body.properties = {};
+			options.body.restype_id = model.id;
+
+			/* exemple :
+			input data:
+			{
+			    "status": 0,
+			    "resource": {
+			    	"familyName": [ "Jaouen" ],
+			    	"givenName": [ "Loïc" ]
+			    }
+			}
+
+			output data:
+			options.body = {
+				"restype_id": "http://www.knora.org/ontology/0108#Author",
+				"properties": {
+					"http://www.knora.org/ontology/0108#hasFamilyName":
+						[{"richtext_value": {"utf8str": "Jaouen"}}],
+					"http://www.knora.org/ontology/0108#hasGivenName":
+						[{"richtext_value": {"utf8str": "Loïc"}}]
+				},
+				"project_id": "http://data.knora.org/projects/0108",
+				"label": "loic jaouen"
+			};
+			*/
+
+			logdebug("data: %o", data);
+			_.forEach(data.resource, function (values, resourceKey) {
+				if (values.length === 0) {
+					return;
+				}
+
+				// resourceKey : "familyName", match with model
+				// propertyName : "http://www.knora.org/ontology/0108#hasFamilyName"
+				let propertyName = model.properties[resourceKey];
+				// find the data type : "text"
+				//logdebug("model.id: %o, resourceKey: %o, propertyName: %o", model.id, resourceKey, propertyName);
+				//logdebug("resource_types: %o", knora.resource_types);
+				//logdebug("resource_type model: %o", knora.resource_types[model.id]);
+				//logdebug("resource_type model prop: %o", knora.resource_types[model.id][propertyName]);
+				let propertyType = knora.resource_types[model.id][propertyName].gui_name;
+				let outValues = [];
+				//logdebug("resource_type proptype: %o", propertyType);
+				switch (propertyType) {
+					case 'text':
+						_.forEach(values, function (value) {
+							outValues.push({"richtext_value": {"utf8str": value}});
+						});
+					//case: 'richtext':
+					//case: 'searchbox':
+					//
+				}
+				options.body.properties[propertyName] = outValues;
+			});
+
+			// TODO: add a label field
+			options.body.label = data.label;
+			// TODO: on login, cache the project id with the auth tocken
+			options.body.project_id = "http://data.knora.org/projects/0108";
+
+			options.json = true;
+		}
 
 		logdebug('request options: %o', options);
 		request(options, function (error, response, body) {
@@ -126,7 +294,13 @@ Knora.prototype.knora_request = function (options, model) {
 
 			// is the response a json object?
 			try {
-				let parsedBody = JSON.parse(body);
+				let parsedBody = body;
+				if (typeof body === 'string' || body instanceof String) {
+					logdebug("typeof body: %o", typeof body);
+					parsedBody = JSON.parse(body);
+				}
+				logdebug("json returned: %j", parsedBody);
+
 				// deal with a json object
 				toReturn.status = parsedBody.status || knora.configKnora.statusCode.ok;
 				let message = parsedBody.message || parsedBody.error || undefined;
@@ -141,14 +315,28 @@ Knora.prototype.knora_request = function (options, model) {
 				}
 				if (parsedBody.props) {
 					toReturn.props = parsedBody.props;
+				//} else {
+				//	if (parsedBody.results) {
+				//		toReturn.props = parsedBody.results;
+				//	}
 				}
 				if (toReturn.status !== knora.configKnora.statusCode.ok) {
+					logdebug("filling on status: %o", toReturn.status);
 					toReturn.message = message;
 					fulfill(toReturn);
 					return;
 				}
-			} catch (e) {
+				if (parsedBody.res_id) {
+					logdebug("filling on res_id: %o", parsedBody.res_id);
+					toReturn.res_id = parsedBody.res_id;
+					fulfill(toReturn);
+					return;
+				}
+			}
+			catch
+				(e) {
 				// the result is not a json object
+				logdebug("filling on exception (no json): %o, %o", e, body);
 				toReturn.status = knora.configKnora.statusCode.other;
 				toReturn.message = body;
 				fulfill(toReturn);
@@ -158,6 +346,7 @@ Knora.prototype.knora_request = function (options, model) {
 			logdebug("props of model: %o", model);
 
 			if (!model) {
+				logdebug("filling on no model");
 				fulfill(toReturn);
 				return;
 			}
@@ -174,7 +363,7 @@ Knora.prototype.knora_request = function (options, model) {
 
 			toReturn.resource = {};
 			// iterate over the properties of the model
-			_.forEach(model, function (value, key) {
+			_.forEach(model.properties, function (value, key) {
 				// key   : title
 				// value : http://www.knora.org/ontology/0108#articleHasTitle'
 				// ---------- or
@@ -215,7 +404,7 @@ Knora.prototype.knora_request = function (options, model) {
 						// link
 						case "http://www.knora.org/ontology/knora-base#LinkValue":
 							let linkedOptions = _.clone(options);
-							for (var i = 0; i < toReturn.props[value].values.length; i++) {
+							for (let i = 0; i < toReturn.props[value].values.length; i++) {
 								linkedOptions.url = knora.getUrlIri("resources", toReturn.props[value].values[i]);
 								logdebug("cooool %o", linkedOptions.url);
 
@@ -237,7 +426,7 @@ Knora.prototype.knora_request = function (options, model) {
 					.then(function (results) {
 						logdebug("all subrequests have returned %s", results.length);
 						// merge the answers
-						for (var i = 0; i < results.length; i++) {
+						for (let i = 0; i < results.length; i++) {
 							// TODO: merge states and messages
 							logdebug("subreq result %n: %o => %o", 1, anchors[i], results[i].resource);
 							toReturn.resource[anchors[i]].push(results[i].resource);
@@ -275,14 +464,20 @@ Knora.prototype.api_request = function (options, req, res, model) {
 	// make this available in request call
 	let knora = this;
 
-	this.fixCookies(options, req);
-
-	logdebug('sending request');
-	// send the request
-
-	this.knora_request(options, model)
+	// make sure that we know the model's type
+	this.knora_restypes(model)
+	// then proceed with the request
 		.then(function (result) {
+			logdebug("types known: %o", result);
 
+			knora.fixCookies(options, req);
+
+			logdebug('sending request, body: %o', req.body);
+
+			return knora.knora_request(options, model, req.body);
+		})
+		// then return the result
+		.then(function (result) {
 			logdebug("success!!! %o", result);
 
 			/* clean up result from internal states */
@@ -299,6 +494,7 @@ Knora.prototype.api_request = function (options, req, res, model) {
 			res.status(HttpStatus.OK);
 			res.json(result);
 		})
+		// or complain about error
 		.catch(function (error) {
 			logdebug("catching an error :(", error);
 			if (error.status) {
@@ -314,7 +510,8 @@ Knora.prototype.api_request = function (options, req, res, model) {
 				});
 			}
 		});
-};
+}
+;
 
 /**
  * Login
