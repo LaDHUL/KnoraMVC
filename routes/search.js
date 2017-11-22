bef47c6b9a6f2a2807012b6c3d0c499b9b6b6067
Loïc Jaouen <loic.jaouen@unil.ch>
2017-11-22 10:49:40 +0100

    fix search after last release's refactoring

--- routes/search.js
+++ routes/search.js
@@ -15,13 +15,14 @@ var knora = new Knora();
  *
  */
 router.use('/', function timelog(req, res, next) {
-	logdebug('received resource request at %s, params: %o', Date.now(), req.params);
+	logdebug('received search request at %s, params: %o', Date.now(), req.params);
 	next();
 });
 
 router.get('/:project/:model', function(req, res) {
+    logdebug("project: %o, model: %o", req.params.project , req.params.model);
 	let model = require('./../models/'+ req.params.project +'/'+ req.params.model);
-	knora.api_search_by_type(knora.configKnora.project[req.params.project], model.model[req.params.model], req, res);
+	knora.api_search_by_type(req.params.project, model.model[req.params.model], req, res);
 });
 
 module.exports = router;
